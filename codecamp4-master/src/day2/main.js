7c7c661a8d1b668ffb1934396d234e5e946db610
NatsuNyo <42639800+BlueMagnolia@users.noreply.github.com>
2019-10-23 19:20:36 +0700

    Update main.js

--- codecamp4-master/src/day2/main.js
+++ codecamp4-master/src/day2/main.js
@@ -15,7 +15,7 @@ function main(str1, str2) {
 
         for (let i = 0; i < str1.length; i++) {
             // console.log(`i=${i}`)
-            console.log(`str1=${str1[i]} , str2=${str2[i]}`)
+            // console.log(`str1=${str1[i]} , str2=${str2[i]}`)
             this.count = (str1[i] !== str2[i]) ? console.log(this.countStr = this.countStr + 1) : this.countStr
         }
         return this.countStr;
